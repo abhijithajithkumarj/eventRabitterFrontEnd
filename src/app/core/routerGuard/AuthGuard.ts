396719979bbba983a699b7debe82cae38e9dac97
ABHIJITH <aabhivishnu@gmail.com>
2024-06-22 12:59:24 +0530

    chang auth in class leavel

--- src/app/core/routerGuard/AuthGuard.ts
+++ src/app/core/routerGuard/AuthGuard.ts
@@ -1,13 +1,14 @@
-import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
-export const AuthGuard:CanActivateFn=() => {
-  const router =inject(Router)
-  if(localStorage.getItem("user")){
+export const AuthGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  console.log('AuthGuard#canActivate called');
+  if (localStorage.getItem('user')) {
+    console.log('User authenticated');
     return true;
   }
+  console.log('User not authenticated, redirecting to login');
   router.navigate(['/login']);
   return false;
-  
-}
-
+};
