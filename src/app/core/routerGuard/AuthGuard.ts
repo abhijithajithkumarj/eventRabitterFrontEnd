bf8e502c3f822c80b5c3b6e8e16ef8dea26640d4
ABHIJITH <aabhivishnu@gmail.com>
2024-06-22 13:33:06 +0530

    reset

--- src/app/core/routerGuard/AuthGuard.ts
+++ src/app/core/routerGuard/AuthGuard.ts
@@ -1,19 +1,12 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthserviceService } from '../service/auth/authservice.service';
+import { inject } from "@angular/core";
+import { CanActivateFn, Router } from "@angular/router";
 
-@Injectable({
-  providedIn: 'root' 
-})
-export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private tokenService: AuthserviceService) {}
-  canActivate(): boolean {
-    const jwtTok = this.tokenService.getToken(); 
-    if (jwtTok) {
-      return true; 
-    } else {
-      this.router.navigate(['/login']); 
-      return false;
-    }
+
+export const AuthGuard:CanActivateFn = ()=> {
+  const router = inject(Router)
+  if(localStorage.getItem('user')){
+      return true;
   }
+  router.navigate(['/login'])
+  return false;
 }
