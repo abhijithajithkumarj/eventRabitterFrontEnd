33e0182ff359849c1e16abedc5fae5b04cd7a144
ABHIJITH <aabhivishnu@gmail.com>
2024-07-26 21:14:33 +0530

    change the nav system

--- src/app/pages/reuse/navigation/navigation.component.ts
+++ src/app/pages/reuse/navigation/navigation.component.ts
@@ -1,85 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthserviceService } from '../../../core/service/auth/authservice.service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css',
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
 
+  roll: string | undefined;
+  userId: string | undefined;
 
   constructor(
     private service: AuthserviceService, 
     private router: Router,
-    private route:ActivatedRoute
+    private route: ActivatedRoute
   ) {}
-
-  roll: string;
-  userId:string
-
-  getUserData!:string;
 
   ngOnInit(): void {
     this.roll = this.service.getUserRoleLocalStorage();
-    this.userId=this.service.getUserFromLocalStorage();
+    this.userId = this.service.getUserFromLocalStorage();
     console.log(this.roll);
   }
 
   navigateRolleBase() {
     if (this.roll === 'USER') {
       this.router.navigateByUrl('controller');
-    }
-    else if (this.roll === 'ADMIN') {
-      this.router.navigateByUrl('adimdashbord');
-    } 
-    else {
+    } else if (this.roll === 'ADMIN') {
+      this.router.navigateByUrl('adminDashboard');
+    } else {
       this.router.navigateByUrl('login');
     }
-
     console.log(this.roll);
-    
   }
 
   profilePage() {
-    if (this.userId !== undefined) {
+    if (this.service.getTokenLocalStorage()) {
       this.router.navigate(['/userpage'], { queryParams: { id: this.userId } })
-        .then(() => {
-          window.location.reload();
-        });
+        .then(() => window.location.reload());
     } else {
       this.router.navigateByUrl('login')
-        .then(() => {
-          window.location.reload();
-        });
+        .then(() => window.location.reload());
     }
   }
 
   eventCreat() {
-    if (this.roll !== undefined) {
+    if (this.service.getTokenLocalStorage()) {
       this.router.navigateByUrl('eventType');
     } else {
       this.router.navigateByUrl('login');
     }
-     this.service.gerUserDataForEventCreate().subscribe((data)=>{
-      this.getUserData=data;
-      console.log(this.getUserData);
-     })
+    this.service.gerUserDataForEventCreate().subscribe((data) => {
+      console.log(data);
+    });
   }
-
 
   eventCard() {
     this.router.navigateByUrl('eventTicketPlace');
+  }
+
+  eventHome() {
+    if (this.service.getTokenLocalStorage()) {
+      this.router.navigateByUrl('home');
+    } else {
+      this.router.navigateByUrl('login');
     }
-
-
-
-    eventHome() {
-      if (this.roll !== undefined) {
-        this.router.navigateByUrl('home');
-      } else {
-        this.router.navigateByUrl('login');
-      }
-      }
+  }
 }
