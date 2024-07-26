b38653f2b229d54ad3b6c317400266777f207281
ABHIJITH <aabhivishnu@gmail.com>
2024-07-26 21:45:32 +0530

    change carosal problem

--- src/app/pages/reuse/carousel/carousel.component.ts
+++ src/app/pages/reuse/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthserviceService } from '../../../core/service/auth/authservice.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-carousel',
@@ -8,6 +9,35 @@ import { AuthserviceService } from '../../../core/service/auth/authservice.servi
 })
 export class CarouselComponent {
 
+
+
+  
+  roll: string | undefined;
+  userId: string | undefined;
+
+  constructor(
+    private service: AuthserviceService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.roll = this.service.getUserRoleLocalStorage();
+    this.userId = this.service.getUserFromLocalStorage();
+    console.log(this.roll);
+  }
+
+
+  eventCreat() {
+    if (this.service.getTokenLocalStorage()) {
+      this.router.navigateByUrl('eventType');
+    } else {
+      this.router.navigateByUrl('login');
+    }
+    this.service.gerUserDataForEventCreate().subscribe((data) => {
+      console.log(data);
+    });
+  }
 
 
   
